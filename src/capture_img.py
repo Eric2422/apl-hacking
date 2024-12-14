768dabd32ec57d09eba82d9944f8bfe68c505494
Eric Zhang <ericbkzhang@gmail.com>
2024-12-14 13:10:36 -0500

    Add a spacebar listener to control whether to save the images during image capture

--- src/capture_img.py
+++ src/capture_img.py
@@ -3,13 +3,16 @@ import sys
 
 import cv2
 
-if len(sys.argv) < 2:
+try:
+    write_dir = sys.argv[1]
+
+    if sys.argv[1] != 'positive' and sys.argv[1] != 'negative':
+        raise ValueError()
+
+except IndexError | ValueError:
     raise ValueError('Passs in either "positive" or "negative"')
 
-if sys.argv[2] != 'positive' and sys.argv[2] != 'negative':
-    raise ValueError('Passs in either "positive" or "negative"')
 
-write_dir = sys.argv[2]
 
 # Get input from camera
 capture = cv2.VideoCapture(0)
@@ -20,6 +23,9 @@ if not capture.isOpened():
 
 print('Camera running...')
 
+# When True, write images
+capture_images = False
+
 while True:
     # Get status and frame from camera
     cap_return, frame = capture.read()
@@ -29,16 +35,27 @@ while True:
         continue
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
     cv2.imshow('frame', gray)
-    cv2.imwrite(f'./{write_dir}/{datetime.now()}.jpg', gray)
+    
+    print(f'Currently{' ' if capture_images else ' *NOT* '}recording.')
+
+    if capture_images:
+        cv2.imwrite(f'./{write_dir}/{datetime.now()}.jpg', gray)
 
     # Wait one millisecond and return the key pressed
     key = cv2.waitKey(1)
 
-    # 27 is the ASCII code for escape
-    if key == 27 or key == ord('q'):
-        break
+    match int(key):
+        # 27 is the ASCII code for escape
+        # 113 is the code for "q"
+        case int(27 | 113):
+            break
+
+        # Start/stop capturing when spacebar is hit
+        # 32 is the code for " "(spacebar)
+        case 32:
+            capture_images = not capture_images
 
 cv2.destroyAllWindows()
 capture.release()
