cc42070a244edeeae863eb0639cfa47f0f213aa8
Eric Zhang <ericbkzhang@gmail.com>
2024-12-14 10:55:12 -0500

    Set up video capture

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import sys
+from time import sleep
+
+import cv2
+import keyboard
+
+capture = cv2.VideoCapture(0)
+
+if not capture.isOpened():
+    print('Failed...')
+    sys.exit()
+
+print('Camera running...')
+
+while True:
+    cap_return, frame = capture.read()
+
+    if not cap_return:
+        print('Failed to retrieve frame')
+        continue
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
+
+    key = cv2.waitKey(1)
+    
+    # 27 is the ASCII code for escape
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+capture.release    
